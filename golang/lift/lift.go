366156d353543995ddfd6c29c3b77bdb18aa68ae
Cameron Wood <me@cameronewood.xyz>
2021-07-04 13:41:52 +0200

    Add NewRequest func to Lift

--- golang/lift/lift.go
+++ golang/lift/lift.go
@@ -1,5 +1,9 @@
 package lift
 
+import (
+	"errors"
+)
+
 // Direction ..
 type Direction int
 
@@ -21,6 +25,65 @@ type Lift struct {
 	Floor     int
 	Requests  []int
 	DoorsOpen bool
+}
+
+// NewRequest handles adding a new request to the queue,
+//  which it maintains in a kind of balanced/pivot list.
+//  Whereby depending on the direction of the lift, the
+//  items will be ordered in ascending order, until an
+//  item is request which is below the current floor. In
+//  that case the item will be added at the end of the
+//  the list in descending order. Here is an example of
+//  what this would look like...
+//
+//  going up => 4, 5, 6, 2, 1, 0
+//  going down => 6, 5, 4, 3, 8, 9
+func (l *Lift) NewRequest(req int) {
+	var i int
+
+	if req == l.Floor {
+		// Check if req is the current floor
+		return
+	} else if len(l.Requests) == 0 {
+		// Check if requests is empty, i.e. append
+		i = 0
+	} else {
+		for index, value := range l.Requests {
+			if l.Requests[0] > l.Floor {
+				if req <= value && req > l.Floor && value > l.Floor {
+					i = index
+					break
+				} else if req > value && value < l.Floor {
+					i = index
+					break
+				}
+			} else if l.Requests[0] < l.Floor {
+				if req >= value && req < l.Floor && value < l.Floor {
+					i = index
+					break
+				} else if req < value && value > l.Floor {
+					i = index
+					break
+				}
+			}
+
+			if index+1 == len(l.Requests) {
+				// End of requests, i.e. must append
+				i = len(l.Requests)
+				break
+			}
+		}
+	}
+
+	if i == len(l.Requests) {
+		// Appending
+		l.Requests = append(l.Requests, req)
+	} else if l.Requests[i] != req {
+		// Inserting
+		l.Requests = append(l.Requests, 0)
+		copy(l.Requests[i+1:], l.Requests[i:])
+		l.Requests[i] = req
+	}
 }
 
 // System ..
---
+++ golang/lift/lift_test.go
@@ -0,0 +1,87 @@
+package lift
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewRequest(t *testing.T) {
+	var tests = []struct {
+		name     string
+		floor    int
+		input    []int
+		expected []int
+	}{
+		{
+			"upwards mixed order",
+			0,
+			[]int{3, 1, 5, 2, 4},
+			[]int{1, 2, 3, 4, 5},
+		},
+		{
+			"upwards sorted order",
+			0,
+			[]int{4, 5, 6, 7, 8},
+			[]int{4, 5, 6, 7, 8},
+		},
+		{
+			"upwards reversed order",
+			0,
+			[]int{9, 8, 7, 6, 5},
+			[]int{5, 6, 7, 8, 9},
+		},
+		{
+			"upwards mixed order with reverse",
+			3,
+			[]int{4, 2, 1, 5, 0},
+			[]int{4, 5, 2, 1, 0},
+		},
+		{
+			"downwards mixed order",
+			6,
+			[]int{3, 1, 5, 2, 4},
+			[]int{5, 4, 3, 2, 1},
+		},
+		{
+			"downwards sorted order",
+			7,
+			[]int{2, 3, 4, 5, 6},
+			[]int{6, 5, 4, 3, 2},
+		},
+		{
+			"downwards reversed order",
+			10,
+			[]int{5, 6, 7, 8, 9},
+			[]int{9, 8, 7, 6, 5},
+		},
+		{
+			"downwards mixed order with reverse",
+			6,
+			[]int{4, 7, 5, 9, 8},
+			[]int{5, 4, 7, 8, 9},
+		},
+		{
+			"basement floors ordered",
+			5,
+			[]int{4, 3, 0, -1, -2},
+			[]int{4, 3, 0, -1, -2},
+		},
+		{
+			"basement floors unorder",
+			5,
+			[]int{4, -1, 3, -2, 0},
+			[]int{4, 3, 0, -1, -2},
+		},
+	}
+
+	for _, test := range tests {
+		lift := Lift{test.name, test.floor, []int{}, true}
+		for _, request := range test.input {
+			lift.NewRequest(request)
+		}
+
+		if !reflect.DeepEqual(lift.Requests, test.expected) {
+			t.Errorf("%s: wanted '%v' but got '%v'\n", test.name, test.expected, lift.Requests)
+		}
+	}
+}
